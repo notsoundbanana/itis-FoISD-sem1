914838475f6750d15a1236291dbfdbd2e79eaf7f
notSoundBanana <chemaev.ds@gmail.com>
2022-09-09 01:39:01 +0300

    Added HEAD, PUT & DELETE methods

--- HTTP-Requests/src/main/java/ru/kpfu/itis/chemaev/net/NetSample.java
+++ HTTP-Requests/src/main/java/ru/kpfu/itis/chemaev/net/NetSample.java
@@ -1,16 +1,14 @@
 package ru.kpfu.itis.chemaev.net;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 public class NetSample {
-    public static void main(String[] args) {
+    public static void get() {
         try {
             URL url = new URL("https://jsonplaceholder.typicode.com/posts?userId=1");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -38,8 +36,9 @@ public class NetSample {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
 
-        // post
+    public static void post() {
         try {
             URL postUrl = new URL("https://gorest.co.in/public/v1/users");
             HttpURLConnection postConnection = (HttpURLConnection) postUrl.openConnection();
@@ -81,4 +80,96 @@ public class NetSample {
             throw new RuntimeException(e);
         }
     }
+
+    public static void head() {
+        try {
+            URL url = new URL("https://jsonplaceholder.typicode.com/posts?userId=1");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("HEAD");
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+
+            System.out.println(connection.getResponseCode());
+
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void put() {
+        try {
+            URL putURL = new URL("https://gorest.co.in/public/v1/users");
+
+            HttpURLConnection putConnection = (HttpURLConnection) putURL.openConnection();
+            String token = "3247df66c027309df362aad144e8229d3333347d2c356a4a616c2f5a81847244";
+
+            putConnection.setRequestMethod("PUT");
+            putConnection.setRequestProperty("Content-Type", "application/json");
+            putConnection.setRequestProperty("Accept", "application/json");
+            putConnection.setRequestProperty("Authorization", "Bearer " + token);
+
+            putConnection.setDoOutput(true);
+
+            String jsonInputString = "{\"name\":\"Danil Chemaev\", \"gender\":\"male\", \"email\":\"tenali.asaramakrishna2@gmail.com\", \"status\":\"active\"}";
+
+            try (OutputStream outputStream = putConnection.getOutputStream()) {
+                byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
+                outputStream.write(input, 0, input.length);
+            }
+
+            putConnection.getInputStream();
+
+//            String jsonInputString = "{\"name\":\"Danil Chemaev\", \"gender\":\"male\", \"email\":\"tenali.asaramakrishna2@gmail.com\", \"status\":\"active\"}";
+
+            try (OutputStream outputStream = putConnection.getOutputStream()) {
+                byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
+                outputStream.write(input, 0, input.length);
+            }
+
+            System.out.println(putConnection.getResponseCode());
+
+
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (ProtocolException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void delete() {
+        try {
+            URL postUrl = new URL("https://gorest.co.in/public/v1/users");
+            HttpURLConnection postConnection = (HttpURLConnection) postUrl.openConnection();
+            String token = "3247df66c027309df362aad144e8229d3333347d2c356a4a616c2f5a81847244";
+
+            postConnection.setRequestMethod("POST");
+            HttpURLConnection connection = (HttpURLConnection) postUrl.openConnection();
+            connection.setRequestMethod("DELETE");
+            int responseCode = connection.getResponseCode();
+        } catch (ProtocolException e) {
+            throw new RuntimeException(e);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        get();
+        post();
+        head();
+        put();
+        delete();
+
+    }
+
 }
